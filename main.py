75aeef65bb908b26fe7ac9370c8c8534918c3dd3
fig-curative <jamielynnkitchens@gmail.com>
2025-08-06 23:30:45 -0600

    player

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import pygame
 from constants import *
-
-class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-        
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0,0)
-        self.radius = radius
-
-    def draw(self, screen):
-        pass
-
-    def update(self, dt):
-        pass
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -30,6 +16,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
