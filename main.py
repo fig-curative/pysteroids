cf5530a567817c700d2c1f596c043bc040f64b41
fig-curative <jamielynnkitchens@gmail.com>
2025-08-07 00:25:40 -0600

    bullets part 1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from circleshape import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -14,10 +15,12 @@ def main():
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
     asteroidables = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatables, drawables)
     Asteroid.containers = (updatables, drawables, asteroidables)
     AsteroidField.containers = (updatables)
+    Shot.containers = (shots, updatables, drawables)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
